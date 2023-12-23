ef3a730a9aa10665e4106f5efce77e1e8dd6d0dc
Loc Nguyen <vinhloc30796@gmail.com>
2023-12-23 17:56:49 +0700

    Decode integers e.g. i52e

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,31 @@ use std::env;
 // Available if you need it!
 // use serde_bencode
 
+fn decode_bencoded_string(encoded_value: &str) -> serde_json::Value {
+    // Example: "5:hello" -> "hello"
+    let colon_index = encoded_value.find(':').unwrap();
+    let number_string = &encoded_value[..colon_index];
+    let number = number_string.parse::<i64>().unwrap();
+    let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+    return serde_json::Value::String(string.to_string());
+}
+
+fn decode_bencoded_integer(encoded_value: &str) -> serde_json::Value {
+    // Example: "i3e" -> 3
+    let end_index = encoded_value.find('e').unwrap();
+    let number_string = &encoded_value[1..end_index];
+    let number = number_string.parse::<i64>().unwrap();
+    return serde_json::Value::Number(serde_json::Number::from(number));
+}
+
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    let first_char = encoded_value.chars().next().unwrap();
+    match first_char {
+        '0'..='9' => return decode_bencoded_string(encoded_value),
+        'i' => return decode_bencoded_integer(encoded_value),
+        _ => panic!("Unhandled bencoded value: {}", encoded_value),
     }
 }
 
