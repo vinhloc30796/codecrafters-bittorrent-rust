2264b4b5f8dc464ca6aae1d658d6d2dc1e5a2c72
Loc Nguyen <vinhloc30796@gmail.com>
2023-12-25 02:17:01 +0700

    Decode bencoded list and dict with u8

--- src/main.rs
+++ src/main.rs
@@ -57,16 +57,22 @@ fn decode_bencoded_integer<T: AsRef<[u8]>>(encoded_value: T) -> (usize, serde_js
 // Example: "l5:helloi3ee" -> ["hello", 3]
 // Example 2: "l4:spam4:eggse" -> ["spam", "eggs"]
 // Example 3: "l4:spaml1:a1:bee" -> ["spam", ["a", "b"]]
-fn decode_bencoded_list(encoded_value: &str) -> (usize, serde_json::Value) {
+fn decode_bencoded_list<T: AsRef<[u8]>>(encoded_value: T) -> (usize, serde_json::Value) {
     // Get string from start until 'e'
-    let mut list = Vec::new();
+    let encoded_value = encoded_value.as_ref();
     let mut encoded_value = &encoded_value[1..];
+    let mut list = Vec::new();
     let mut ending_index = 1;
-    while encoded_value.chars().next().unwrap() != 'e' {
-        let (child_index, decoded_value) = decode_bencoded_value(encoded_value);
-        list.push(decoded_value);
-        encoded_value = &encoded_value[child_index..];
-        ending_index += child_index;
+    loop {
+        match encoded_value.iter().next().unwrap() {
+            b'e' => break,
+            _ => {
+                let (child_index, decoded_value) = decode_bencoded_value(encoded_value);
+                list.push(decoded_value);
+                encoded_value = &encoded_value[child_index..];
+                ending_index += child_index;
+            }
+        }
     }
     ending_index += 1;
     return (ending_index, serde_json::Value::Array(list));
@@ -76,34 +82,42 @@ fn decode_bencoded_list(encoded_value: &str) -> (usize, serde_json::Value) {
 // Example 2: "d4:spaml1:a1:bee" -> {"spam": ["a", "b"]}
 // Example 3: "d4:foodd1:a3:baree" -> {"food": {"a": "bar"}}
 // -> {"publisher": "bob", "publisher-webpage": "www.example.com", "publisher.location": "home"}
-fn decode_bencoded_dict(encoded_value: &str) -> (usize, serde_json::Value) {
+fn decode_bencoded_dict<T: AsRef<[u8]>>(encoded_value: T) -> (usize, serde_json::Value) {
     // Get string from start until 'e'
-    let mut dict = serde_json::Map::new();
+    let encoded_value = encoded_value.as_ref();
     let mut encoded_value = &encoded_value[1..];
     let mut ending_index = 1;
-    while encoded_value.chars().next().unwrap() != 'e' {
-        let (key_index, key) = decode_bencoded_string(encoded_value);
-        encoded_value = &encoded_value[key_index..];
-        ending_index += key_index;
-        let (value_index, value) = decode_bencoded_value(encoded_value);
-        encoded_value = &encoded_value[value_index..];
-        ending_index += value_index;
-        dict.insert(key.as_str().unwrap().to_string(), value);
+    let mut dict = serde_json::Map::new();
+    loop {
+        match encoded_value.iter().next().unwrap() {
+            b'e' => break,
+            _ => {
+                let (key_index, key) = decode_bencoded_string(encoded_value);
+                encoded_value = &encoded_value[key_index..];
+                ending_index += key_index;
+                let (value_index, value) = decode_bencoded_value(encoded_value);
+                encoded_value = &encoded_value[value_index..];
+                ending_index += value_index;
+                dict.insert(key.as_str().unwrap().to_string(), value);
+            }
+        }
     }
     ending_index += 1;
     return (ending_index, serde_json::Value::Object(dict));
 }
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> (usize, serde_json::Value) {
+fn decode_bencoded_value<T: AsRef<[u8]> + std::fmt::Debug>(
+    encoded_value: T,
+) -> (usize, serde_json::Value) {
     // If encoded_value starts with a digit, it's a number
-    let first_char = encoded_value.chars().next().unwrap();
+    let first_char = encoded_value.as_ref()[0] as char;
     match first_char {
         '0'..='9' => return decode_bencoded_string(encoded_value),
         'i' => return decode_bencoded_integer(encoded_value),
         'l' => return decode_bencoded_list(encoded_value),
         'd' => return decode_bencoded_dict(encoded_value),
-        _ => panic!("Unhandled bencoded value: {}", encoded_value),
+        _ => panic!("Unhandled bencoded value: {:?}", encoded_value),
     }
 }
 
