82a23b77c4303513cee439f2c73e9a4b08d697ed
Loc Nguyen <vinhloc30796@gmail.com>
2023-12-24 10:46:53 +0700

    Decode bencoded dicts e.g. d3:cow3:moo4:spam4:eggse

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,28 @@ fn decode_bencoded_list(encoded_value: &str) -> (usize, serde_json::Value) {
     return (ending_index, serde_json::Value::Array(list));
 }
 
+// Example: "d3:cow3:moo4:spam4:eggse" -> {"cow": "moo", "spam": "eggs"}
+// Example 2: "d4:spaml1:a1:bee" -> {"spam": ["a", "b"]}
+// Example 3: "d4:foodd1:a3:baree" -> {"food": {"a": "bar"}}
+// -> {"publisher": "bob", "publisher-webpage": "www.example.com", "publisher.location": "home"}
+fn decode_bencoded_dict(encoded_value: &str) -> (usize, serde_json::Value) {
+    // Get string from start until 'e'
+    let mut dict = serde_json::Map::new();
+    let mut encoded_value = &encoded_value[1..];
+    let mut ending_index = 1;
+    while encoded_value.chars().next().unwrap() != 'e' {
+        let (key_index, key) = decode_bencoded_string(encoded_value);
+        encoded_value = &encoded_value[key_index..];
+        ending_index += key_index;
+        let (value_index, value) = decode_bencoded_value(encoded_value);
+        encoded_value = &encoded_value[value_index..];
+        ending_index += value_index;
+        dict.insert(key.as_str().unwrap().to_string(), value);
+    }
+    ending_index += 1;
+    return (ending_index, serde_json::Value::Object(dict));
+}
+
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> (usize, serde_json::Value) {
     // If encoded_value starts with a digit, it's a number
@@ -56,6 +78,7 @@ fn decode_bencoded_value(encoded_value: &str) -> (usize, serde_json::Value) {
         '0'..='9' => return decode_bencoded_string(encoded_value),
         'i' => return decode_bencoded_integer(encoded_value),
         'l' => return decode_bencoded_list(encoded_value),
+        'd' => return decode_bencoded_dict(encoded_value),
         _ => panic!("Unhandled bencoded value: {}", encoded_value),
     }
 }
